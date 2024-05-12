0ed31f63d047dd8a7f759620092593917ede9c23
Tuyishimeeric <51074730+Tuyishimeeric@users.noreply.github.com>
2024-05-12 14:36:08 +0200

    weather

---
+++ week03/scripts/hero.js
@@ -0,0 +1,23 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    function calculateWindChill(temperature, windSpeed) {
+        if (temperature <= 10 && windSpeed > 4.8) {
+            return Math.round(13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16)));
+        } else {
+            return "N/A";
+        }
+    }
+
+
+    const temperature = 5;
+    const windSpeed = 10; 
+    const windchill = calculateWindChill(temperature, windSpeed);
+    document.getElementById("windchill").textContent = windchill;
+
+    const currentYear = new Date().getFullYear();
+    const lastModifiedDate = document.lastModified;
+    document.getElementById("currentYear").textContent = currentYear;
+    document.getElementById("lastModified").textContent = lastModifiedDate;
+});
